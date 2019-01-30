94f8343e71e462834f2c1bffb382834e91b96b7c
nieyc <nieyc@cssweb.sh.cn>
2019-01-30 11:25:07 +0800

    add third dubboservice for client

--- springboot-dubbo-client/src/main/java/com/panchan/cn/springbootdubboclient/controller/DemoConsumerController.java
+++ springboot-dubbo-client/src/main/java/com/panchan/cn/springbootdubboclient/controller/DemoConsumerController.java
@@ -2,6 +2,8 @@ package com.panchan.cn.springbootdubboclient.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.panchan.api.service.DemoService;
+//import com.panchan.cn.metro.pojo.AppVersion;
+//import com.panchan.cn.metro.sei.IAppVersionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +25,29 @@ public class DemoConsumerController {
     @Reference(version = "${demo.service.version}")
     private DemoService demoService;
 
+
+    //@Reference
+    //private IAppVersionService appVersionService;
+
     @RequestMapping("/sayHello/{name}")
     public String sayHello(@PathVariable("name") String name) {
         logger.debug("name in methord sayHello:"+name);
         return demoService.sayHello(name);
     }
+
+    /**
+     * 调用gy-core 提供的接口
+     * @param type
+     * @return
+     */
+    /*@RequestMapping("/getVersion")
+    public Object getVersion(String type) {
+        AppVersion appVersion = null;
+        try {
+            appVersion = appVersionService.getAppVersionByType(type);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return appVersion;
+    }*/
 }
